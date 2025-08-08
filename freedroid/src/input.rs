dodomorandi/freedroid-rs

commit e34e3f3955031447ca6ae4d5675cf92e0f1ccacd

feat: replace cstr with c literal strings

freedroid/src/input.rs
@@ -8,7 +8,6 @@ use crate::{
 };
 
 #[cfg(not(target_os = "android"))]
-use cstr::cstr;
 use log::info;
 use sdl::{
     convert::{i32_to_u8, u32_to_u16},
@@ -85,221 +84,221 @@ const fn create_key_strings() -> [Option<&'static CStr>; PointerStates::Last.to_
     macro_rules! set_out {
         ($(ps::$key:ident = $str:literal);+ $(;)?) => {
             $(
-                out[PointerStates::$key.to_usize()] = Some(cstr!($str));
+                out[PointerStates::$key.to_usize()] = Some($str);
             )+
         };
 
         ($($key:ident = $str:literal);+ $(;)?) => {
             $(
-                out[::sdl::convert::u32_to_usize(sdl_sys::$key)] = Some(cstr!($str));
+                out[::sdl::convert::u32_to_usize(sdl_sys::$key)] = Some($str);
             )+
         };
     }
 
-    out[0] = Some(cstr!("None"));
+    out[0] = Some(c"None");
     if cfg!(feature = "gcw0") {
         set_out!(
-            SDLKey_SDLK_BACKSPACE = "RSldr";
-            SDLKey_SDLK_TAB = "LSldr";
-            SDLKey_SDLK_RETURN = "Start";
-            SDLKey_SDLK_SPACE = "Y";
-            SDLKey_SDLK_ESCAPE = "Select";
+            SDLKey_SDLK_BACKSPACE = c"RSldr";
+            SDLKey_SDLK_TAB = c"LSldr";
+            SDLKey_SDLK_RETURN = c"Start";
+            SDLKey_SDLK_SPACE = c"Y";
+            SDLKey_SDLK_ESCAPE = c"Select";
         );
     }
 
     if cfg!(not(feature = "gcw0")) {
         set_out!(
-            SDLKey_SDLK_BACKSPACE = "BS";
-            SDLKey_SDLK_TAB = "Tab";
-            SDLKey_SDLK_RETURN = "Return";
-            SDLKey_SDLK_SPACE = "Space";
-            SDLKey_SDLK_ESCAPE = "Esc";
+            SDLKey_SDLK_BACKSPACE = c"BS";
+            SDLKey_SDLK_TAB = c"Tab";
+            SDLKey_SDLK_RETURN = c"Return";
+            SDLKey_SDLK_SPACE = c"Space";
+            SDLKey_SDLK_ESCAPE = c"Esc";
         );
     }
 
     set_out!(
-        SDLKey_SDLK_CLEAR = "Clear";
-        SDLKey_SDLK_PAUSE = "Pause";
-        SDLKey_SDLK_EXCLAIM = "!";
-        SDLKey_SDLK_QUOTEDBL = "\"";
-        SDLKey_SDLK_HASH = "#";
-        SDLKey_SDLK_DOLLAR = "$";
-        SDLKey_SDLK_AMPERSAND = "&";
-        SDLKey_SDLK_QUOTE = "'";
-        SDLKey_SDLK_LEFTPAREN = "(";
-        SDLKey_SDLK_RIGHTPAREN = ")";
-        SDLKey_SDLK_ASTERISK = "*";
-        SDLKey_SDLK_PLUS = "+";
-        SDLKey_SDLK_COMMA = ",";
-        SDLKey_SDLK_MINUS = "-";
-        SDLKey_SDLK_PERIOD = ".";
-        SDLKey_SDLK_SLASH = "/";
-        SDLKey_SDLK_0 = "0";
-        SDLKey_SDLK_1 = "1";
-        SDLKey_SDLK_2 = "2";
-        SDLKey_SDLK_3 = "3";
-        SDLKey_SDLK_4 = "4";
-        SDLKey_SDLK_5 = "5";
-        SDLKey_SDLK_6 = "6";
-        SDLKey_SDLK_7 = "7";
-        SDLKey_SDLK_8 = "8";
-        SDLKey_SDLK_9 = "9";
-        SDLKey_SDLK_COLON = ":";
-        SDLKey_SDLK_SEMICOLON = ";";
-        SDLKey_SDLK_LESS = "<";
-        SDLKey_SDLK_EQUALS = "=";
-        SDLKey_SDLK_GREATER = ">";
-        SDLKey_SDLK_QUESTION = "?";
-        SDLKey_SDLK_AT = "@";
-        SDLKey_SDLK_LEFTBRACKET = "[";
-        SDLKey_SDLK_BACKSLASH = "\\";
-        SDLKey_SDLK_RIGHTBRACKET = "]";
-        SDLKey_SDLK_CARET = "^";
-        SDLKey_SDLK_UNDERSCORE = "_";
-        SDLKey_SDLK_BACKQUOTE = "`";
-        SDLKey_SDLK_a = "a";
-        SDLKey_SDLK_b = "b";
-        SDLKey_SDLK_c = "c";
-        SDLKey_SDLK_d = "d";
-        SDLKey_SDLK_e = "e";
-        SDLKey_SDLK_f = "f";
-        SDLKey_SDLK_g = "g";
-        SDLKey_SDLK_h = "h";
-        SDLKey_SDLK_i = "i";
-        SDLKey_SDLK_j = "j";
-        SDLKey_SDLK_k = "k";
-        SDLKey_SDLK_l = "l";
-        SDLKey_SDLK_m = "m";
-        SDLKey_SDLK_n = "n";
-        SDLKey_SDLK_o = "o";
-        SDLKey_SDLK_p = "p";
-        SDLKey_SDLK_q = "q";
-        SDLKey_SDLK_r = "r";
-        SDLKey_SDLK_s = "s";
-        SDLKey_SDLK_t = "t";
-        SDLKey_SDLK_u = "u";
-        SDLKey_SDLK_v = "v";
-        SDLKey_SDLK_w = "w";
-        SDLKey_SDLK_x = "x";
-        SDLKey_SDLK_y = "y";
-        SDLKey_SDLK_z = "z";
-        SDLKey_SDLK_DELETE = "Del";
+        SDLKey_SDLK_CLEAR = c"Clear";
+        SDLKey_SDLK_PAUSE = c"Pause";
+        SDLKey_SDLK_EXCLAIM = c"!";
+        SDLKey_SDLK_QUOTEDBL = c"\"";
+        SDLKey_SDLK_HASH = c"#";
+        SDLKey_SDLK_DOLLAR = c"$";
+        SDLKey_SDLK_AMPERSAND = c"&";
+        SDLKey_SDLK_QUOTE = c"'";
+        SDLKey_SDLK_LEFTPAREN = c"(";
+        SDLKey_SDLK_RIGHTPAREN = c")";
+        SDLKey_SDLK_ASTERISK = c"*";
+        SDLKey_SDLK_PLUS = c"+";
+        SDLKey_SDLK_COMMA = c",";
+        SDLKey_SDLK_MINUS = c"-";
+        SDLKey_SDLK_PERIOD = c".";
+        SDLKey_SDLK_SLASH = c"/";
+        SDLKey_SDLK_0 = c"0";
+        SDLKey_SDLK_1 = c"1";
+        SDLKey_SDLK_2 = c"2";
+        SDLKey_SDLK_3 = c"3";
+        SDLKey_SDLK_4 = c"4";
+        SDLKey_SDLK_5 = c"5";
+        SDLKey_SDLK_6 = c"6";
+        SDLKey_SDLK_7 = c"7";
+        SDLKey_SDLK_8 = c"8";
+        SDLKey_SDLK_9 = c"9";
+        SDLKey_SDLK_COLON = c":";
+        SDLKey_SDLK_SEMICOLON = c";";
+        SDLKey_SDLK_LESS = c"<";
+        SDLKey_SDLK_EQUALS = c"=";
+        SDLKey_SDLK_GREATER = c">";
+        SDLKey_SDLK_QUESTION = c"?";
+        SDLKey_SDLK_AT = c"@";
+        SDLKey_SDLK_LEFTBRACKET = c"[";
+        SDLKey_SDLK_BACKSLASH = c"\\";
+        SDLKey_SDLK_RIGHTBRACKET = c"]";
+        SDLKey_SDLK_CARET = c"^";
+        SDLKey_SDLK_UNDERSCORE = c"_";
+        SDLKey_SDLK_BACKQUOTE = c"`";
+        SDLKey_SDLK_a = c"a";
+        SDLKey_SDLK_b = c"b";
+        SDLKey_SDLK_c = c"c";
+        SDLKey_SDLK_d = c"d";
+        SDLKey_SDLK_e = c"e";
+        SDLKey_SDLK_f = c"f";
+        SDLKey_SDLK_g = c"g";
+        SDLKey_SDLK_h = c"h";
+        SDLKey_SDLK_i = c"i";
+        SDLKey_SDLK_j = c"j";
+        SDLKey_SDLK_k = c"k";
+        SDLKey_SDLK_l = c"l";
+        SDLKey_SDLK_m = c"m";
+        SDLKey_SDLK_n = c"n";
+        SDLKey_SDLK_o = c"o";
+        SDLKey_SDLK_p = c"p";
+        SDLKey_SDLK_q = c"q";
+        SDLKey_SDLK_r = c"r";
+        SDLKey_SDLK_s = c"s";
+        SDLKey_SDLK_t = c"t";
+        SDLKey_SDLK_u = c"u";
+        SDLKey_SDLK_v = c"v";
+        SDLKey_SDLK_w = c"w";
+        SDLKey_SDLK_x = c"x";
+        SDLKey_SDLK_y = c"y";
+        SDLKey_SDLK_z = c"z";
+        SDLKey_SDLK_DELETE = c"Del";
     );
 
     /* Numeric keypad */
     set_out!(
-        SDLKey_SDLK_KP0 = "Num[0 as usize]";
-        SDLKey_SDLK_KP1 = "Num[1 as usize]";
-        SDLKey_SDLK_KP2 = "Num[2 as usize]";
-        SDLKey_SDLK_KP3 = "Num[3 as usize]";
-        SDLKey_SDLK_KP4 = "Num[4 as usize]";
-        SDLKey_SDLK_KP5 = "Num[5 as usize]";
-        SDLKey_SDLK_KP6 = "Num[6 as usize]";
-        SDLKey_SDLK_KP7 = "Num[7 as usize]";
-        SDLKey_SDLK_KP8 = "Num[8 as usize]";
-        SDLKey_SDLK_KP9 = "Num[9 as usize]";
-        SDLKey_SDLK_KP_PERIOD = "Num[. as usize]";
-        SDLKey_SDLK_KP_DIVIDE = "Num[/ as usize]";
-        SDLKey_SDLK_KP_MULTIPLY = "Num[* as usize]";
-        SDLKey_SDLK_KP_MINUS = "Num[- as usize]";
-        SDLKey_SDLK_KP_PLUS = "Num[+ as usize]";
-        SDLKey_SDLK_KP_ENTER = "Num[Enter as usize]";
-        SDLKey_SDLK_KP_EQUALS = "Num[= as usize]";
+        SDLKey_SDLK_KP0 = c"Num[0 as usize]";
+        SDLKey_SDLK_KP1 = c"Num[1 as usize]";
+        SDLKey_SDLK_KP2 = c"Num[2 as usize]";
+        SDLKey_SDLK_KP3 = c"Num[3 as usize]";
+        SDLKey_SDLK_KP4 = c"Num[4 as usize]";
+        SDLKey_SDLK_KP5 = c"Num[5 as usize]";
+        SDLKey_SDLK_KP6 = c"Num[6 as usize]";
+        SDLKey_SDLK_KP7 = c"Num[7 as usize]";
+        SDLKey_SDLK_KP8 = c"Num[8 as usize]";
+        SDLKey_SDLK_KP9 = c"Num[9 as usize]";
+        SDLKey_SDLK_KP_PERIOD = c"Num[. as usize]";
+        SDLKey_SDLK_KP_DIVIDE = c"Num[/ as usize]";
+        SDLKey_SDLK_KP_MULTIPLY = c"Num[* as usize]";
+        SDLKey_SDLK_KP_MINUS = c"Num[- as usize]";
+        SDLKey_SDLK_KP_PLUS = c"Num[+ as usize]";
+        SDLKey_SDLK_KP_ENTER = c"Num[Enter as usize]";
+        SDLKey_SDLK_KP_EQUALS = c"Num[= as usize]";
     );
 
     /* Arrows + Home/End pad */
     set_out!(
-        SDLKey_SDLK_UP = "Up";
-        SDLKey_SDLK_DOWN = "Down";
-        SDLKey_SDLK_RIGHT = "Right";
-        SDLKey_SDLK_LEFT = "Left";
-        SDLKey_SDLK_INSERT = "Insert";
-        SDLKey_SDLK_HOME = "Home";
-        SDLKey_SDLK_END = "End";
-        SDLKey_SDLK_PAGEUP = "PageUp";
-        SDLKey_SDLK_PAGEDOWN = "PageDown";
+        SDLKey_SDLK_UP = c"Up";
+        SDLKey_SDLK_DOWN = c"Down";
+        SDLKey_SDLK_RIGHT = c"Right";
+        SDLKey_SDLK_LEFT = c"Left";
+        SDLKey_SDLK_INSERT = c"Insert";
+        SDLKey_SDLK_HOME = c"Home";
+        SDLKey_SDLK_END = c"End";
+        SDLKey_SDLK_PAGEUP = c"PageUp";
+        SDLKey_SDLK_PAGEDOWN = c"PageDown";
     );
 
     /* Function keys */
     set_out!(
-        SDLKey_SDLK_F1 = "F1";
-        SDLKey_SDLK_F2 = "F2";
-        SDLKey_SDLK_F3 = "F3";
-        SDLKey_SDLK_F4 = "F4";
-        SDLKey_SDLK_F5 = "F5";
-        SDLKey_SDLK_F6 = "F6";
-        SDLKey_SDLK_F7 = "F7";
-        SDLKey_SDLK_F8 = "F8";
-        SDLKey_SDLK_F9 = "F9";
-        SDLKey_SDLK_F10 = "F10";
-        SDLKey_SDLK_F11 = "F11";
-        SDLKey_SDLK_F12 = "F12";
-        SDLKey_SDLK_F13 = "F13";
-        SDLKey_SDLK_F14 = "F14";
-        SDLKey_SDLK_F15 = "F15";
+        SDLKey_SDLK_F1 = c"F1";
+        SDLKey_SDLK_F2 = c"F2";
+        SDLKey_SDLK_F3 = c"F3";
+        SDLKey_SDLK_F4 = c"F4";
+        SDLKey_SDLK_F5 = c"F5";
+        SDLKey_SDLK_F6 = c"F6";
+        SDLKey_SDLK_F7 = c"F7";
+        SDLKey_SDLK_F8 = c"F8";
+        SDLKey_SDLK_F9 = c"F9";
+        SDLKey_SDLK_F10 = c"F10";
+        SDLKey_SDLK_F11 = c"F11";
+        SDLKey_SDLK_F12 = c"F12";
+        SDLKey_SDLK_F13 = c"F13";
+        SDLKey_SDLK_F14 = c"F14";
+        SDLKey_SDLK_F15 = c"F15";
     );
 
     /* Key state modifier keys */
     set_out!(
-        SDLKey_SDLK_NUMLOCK = "NumLock";
-        SDLKey_SDLK_CAPSLOCK = "CapsLock";
-        SDLKey_SDLK_SCROLLOCK = "ScrlLock";
+        SDLKey_SDLK_NUMLOCK = c"NumLock";
+        SDLKey_SDLK_CAPSLOCK = c"CapsLock";
+        SDLKey_SDLK_SCROLLOCK = c"ScrlLock";
     );
     if cfg!(feature = "gcw0") {
         set_out!(
-            SDLKey_SDLK_LSHIFT = "X";
-            SDLKey_SDLK_LCTRL = "A";
-            SDLKey_SDLK_LALT = "B";
+            SDLKey_SDLK_LSHIFT = c"X";
+            SDLKey_SDLK_LCTRL = c"A";
+            SDLKey_SDLK_LALT = c"B";
         );
     }
 
     if cfg!(not(feature = "gcw0")) {
         set_out!(
-            SDLKey_SDLK_LSHIFT = "LShift";
-            SDLKey_SDLK_LCTRL = "LCtrl";
-            SDLKey_SDLK_LALT = "LAlt";
+            SDLKey_SDLK_LSHIFT = c"LShift";
+            SDLKey_SDLK_LCTRL = c"LCtrl";
+            SDLKey_SDLK_LALT = c"LAlt";
         );
     }
 
     set_out!(
-        SDLKey_SDLK_RSHIFT = "RShift";
-        SDLKey_SDLK_RCTRL = "RCtrl";
-        SDLKey_SDLK_RALT = "RAlt";
-        SDLKey_SDLK_RMETA = "RMeta";
-        SDLKey_SDLK_LMETA = "LMeta";
-        SDLKey_SDLK_LSUPER = "LSuper";
-        SDLKey_SDLK_RSUPER = "RSuper";
-        SDLKey_SDLK_MODE = "Mode";
-        SDLKey_SDLK_COMPOSE = "Compose";
+        SDLKey_SDLK_RSHIFT = c"RShift";
+        SDLKey_SDLK_RCTRL = c"RCtrl";
+        SDLKey_SDLK_RALT = c"RAlt";
+        SDLKey_SDLK_RMETA = c"RMeta";
+        SDLKey_SDLK_LMETA = c"LMeta";
+        SDLKey_SDLK_LSUPER = c"LSuper";
+        SDLKey_SDLK_RSUPER = c"RSuper";
+        SDLKey_SDLK_MODE = c"Mode";
+        SDLKey_SDLK_COMPOSE = c"Compose";
     );
 
     /* Miscellaneous function keys */
     set_out!(
-        SDLKey_SDLK_HELP = "Help";
-        SDLKey_SDLK_PRINT = "Print";
-        SDLKey_SDLK_SYSREQ = "SysReq";
-        SDLKey_SDLK_BREAK = "Break";
-        SDLKey_SDLK_MENU = "Menu";
-        SDLKey_SDLK_POWER = "Power";
-        SDLKey_SDLK_EURO = "Euro";
-        SDLKey_SDLK_UNDO = "Undo";
+        SDLKey_SDLK_HELP = c"Help";
+        SDLKey_SDLK_PRINT = c"Print";
+        SDLKey_SDLK_SYSREQ = c"SysReq";
+        SDLKey_SDLK_BREAK = c"Break";
+        SDLKey_SDLK_MENU = c"Menu";
+        SDLKey_SDLK_POWER = c"Power";
+        SDLKey_SDLK_EURO = c"Euro";
+        SDLKey_SDLK_UNDO = c"Undo";
     );
 
     /* Mouse und Joy buttons */
     set_out!(
-        ps::MouseButton1 = "Mouse1";
-        ps::MouseButton2 = "Mouse2";
-        ps::MouseButton3 = "Mouse3";
-        ps::MouseWheelup = "WheelUp";
-        ps::MouseWheeldown = "WheelDown";
-        ps::JoyUp = "JoyUp";
-        ps::JoyDown = "JoyDown";
-        ps::JoyLeft = "JoyLeft";
-        ps::JoyRight = "JoyRight";
-        ps::JoyButton1 = "Joy-A";
-        ps::JoyButton2 = "Joy-B";
-        ps::JoyButton3 = "Joy-X";
-        ps::JoyButton4 = "Joy-Y";
+        ps::MouseButton1 = c"Mouse1";
+        ps::MouseButton2 = c"Mouse2";
+        ps::MouseButton3 = c"Mouse3";
+        ps::MouseWheelup = c"WheelUp";
+        ps::MouseWheeldown = c"WheelDown";
+        ps::JoyUp = c"JoyUp";
+        ps::JoyDown = c"JoyDown";
+        ps::JoyLeft = c"JoyLeft";
+        ps::JoyRight = c"JoyRight";
+        ps::JoyButton1 = c"Joy-A";
+        ps::JoyButton2 = c"Joy-B";
+        ps::JoyButton3 = c"Joy-X";
+        ps::JoyButton4 = c"Joy-Y";
     );
 
     out

freedroid/src/map.rs
@@ -17,7 +17,6 @@ use crate::{
 use arrayvec::ArrayVec;
 use bstr::ByteSlice;
 #[cfg(not(target_os = "android"))]
-use cstr::cstr;
 use defs::MAX_DOORS_ON_LEVEL;
 #[cfg(not(target_os = "android"))]
 use defs::MAX_WP_CONNECTIONS;
@@ -105,13 +104,13 @@ impl Color {
     #[must_use]
     pub const fn c_name(self) -> &'static CStr {
         match self {
-            Color::Red => cstr!("Red"),
-            Color::Yellow => cstr!("Yellow"),
-            Color::Green => cstr!("Green"),
-            Color::Gray => cstr!("Grey"),
-            Color::Blue => cstr!("Blue"),
-            Color::Greenblue => cstr!("Turquoise"),
-            Color::Dark => cstr!("Dark"),
+            Color::Red => c"Red",
+            Color::Yellow => c"Yellow",
+            Color::Green => c"Green",
+            Color::Gray => c"Grey",
+            Color::Blue => c"Blue",
+            Color::Greenblue => c"Turquoise",
+            Color::Dark => c"Dark",
         }
     }
 

Cargo.toml
@@ -13,7 +13,6 @@ authors = ["Edoardo Morandi <morandidodo@gmail.com>"]
 
 [workspace.dependencies]
 bitflags = "2.3.1"
-cstr = "0.2.9"
 log = "0.4.14"
 
 [workspace.lints.rust]

freedroid/Cargo.toml
@@ -16,7 +16,6 @@ bitflags.workspace = true
 bstr = "1.5.0"
 chrono = "0.4.19"
 clap = { version = "4.3.0", features = ["derive", "cargo"] }
-cstr.workspace = true
 env_logger = "0.10.0"
 log.workspace = true
 nom = "7.1.1"

freedroid/src/defs.rs
@@ -1,7 +1,6 @@
 use crate::{input::Input, structs::Point, vars::Vars, Sdl};
 
 use bitflags::bitflags;
-use cstr::cstr;
 use sdl::convert::{u32_to_isize, u32_to_u16};
 #[cfg(feature = "gcw0")]
 use sdl_sys::{
@@ -531,9 +530,9 @@ pub const FD_DATADIR: &str = "."; // our local fallback
 pub const LOCAL_DATADIR: &str = ".."; // local fallback
                                       // #endif
 
-pub const GRAPHICS_DIR_C: &CStr = cstr!("graphics/");
-pub const SOUND_DIR_C: &CStr = cstr!("sound/");
-pub const MAP_DIR_C: &CStr = cstr!("map/");
+pub const GRAPHICS_DIR_C: &CStr = c"graphics/";
+pub const SOUND_DIR_C: &CStr = c"sound/";
+pub const MAP_DIR_C: &CStr = c"map/";
 
 pub const MAP_BLOCK_FILE: &[u8] = b"map_blocks.png";
 pub const DROID_BLOCK_FILE: &[u8] = b"droids.png";
@@ -1022,20 +1021,20 @@ pub enum Status {
 impl Status {
     pub const fn c_name(self) -> &'static CStr {
         match self {
-            Self::Mobile => cstr!("Mobile"),
-            Self::Transfermode => cstr!("Transfer"),
-            Self::Weapon => cstr!("Weapon"),
-            Self::Console => cstr!("Logged In"),
-            Self::Debriefing => cstr!("Debriefing"),
-            Self::Terminated => cstr!("Terminated"),
-            Self::Pause => cstr!("Pause"),
-            Self::Cheese => cstr!("Cheese"),
-            Self::Elevator => cstr!("Elevator"),
-            Self::Briefing => cstr!("Briefing"),
-            Self::Menu => cstr!("Menu"),
-            Self::Victory => cstr!("Victory"),
-            Self::Activate => cstr!("Activate"),
-            Self::Out => cstr!("-- OUT --"),
+            Self::Mobile => c"Mobile",
+            Self::Transfermode => c"Transfer",
+            Self::Weapon => c"Weapon",
+            Self::Console => c"Logged In",
+            Self::Debriefing => c"Debriefing",
+            Self::Terminated => c"Terminated",
+            Self::Pause => c"Pause",
+            Self::Cheese => c"Cheese",
+            Self::Elevator => c"Elevator",
+            Self::Briefing => c"Briefing",
+            Self::Menu => c"Menu",
+            Self::Victory => c"Victory",
+            Self::Activate => c"Activate",
+            Self::Out => c"-- OUT --",
         }
     }
 

freedroid/src/graphics.rs
@@ -20,7 +20,6 @@ use crate::{
 
 use arrayvec::ArrayString;
 use bitflags::bitflags;
-use cstr::cstr;
 use log::{error, info, trace, warn};
 use once_cell::sync::Lazy;
 use sdl::{
@@ -364,7 +363,7 @@ impl crate::Data<'_> {
         drop(rw_ops);
         self.graphics.number_of_screenshot = self.graphics.number_of_screenshot.wrapping_add(1);
         self.display_banner(
-            Some(cstr!("Screenshot")),
+            Some(c"Screenshot"),
             None,
             DisplayBannerFlags::NO_SDL_UPDATE | DisplayBannerFlags::FORCE_UPDATE,
         );
@@ -826,9 +825,7 @@ impl crate::Data<'_> {
             } = self;
 
             /* if there's a window-manager */
-            sdl.video
-                .window_manager()
-                .set_caption(cstr!("Freedroid"), cstr!(""));
+            sdl.video.window_manager().set_caption(c"Freedroid", c"");
             let fpath = Self::find_file_static(
                 global,
                 misc,

freedroid/src/influencer.rs
@@ -8,7 +8,6 @@ use crate::{
     structs::{Bullet, Finepoint, Gps, TextToBeDisplayed},
 };
 
-use cstr::cstr;
 use defs::{Cmds, BLINKENERGY, MAX_INFLU_POSITION_HISTORY, WAIT_TRANSFERMODE};
 use log::{info, warn};
 use rand::{thread_rng, Rng};
@@ -72,14 +71,14 @@ impl crate::Data<'_> {
             // since robots like the refresh, the influencer might also say so...
             if self.global.game_config.droid_talk {
                 self.vars.me.text_to_be_displayed =
-                    TextToBeDisplayed::String(cstr!("Ahhh, that feels so good..."));
+                    TextToBeDisplayed::String(c"Ahhh, that feels so good...");
                 self.vars.me.text_visible_time = 0.;
             }
         } else {
             // If nothing more is to be had, the influencer might also say so...
             if self.global.game_config.droid_talk {
                 self.vars.me.text_to_be_displayed =
-                    TextToBeDisplayed::String(cstr!("Oh, it seems that was it again."));
+                    TextToBeDisplayed::String(c"Oh, it seems that was it again.");
                 self.vars.me.text_visible_time = 0.;
             }
         }

freedroid/src/init.rs
@@ -25,7 +25,6 @@ use crate::input::wait_for_key_pressed;
 
 use bstr::ByteSlice;
 use clap::{crate_version, ArgAction, Parser};
-use cstr::cstr;
 use log::{error, info, warn};
 use nom::Finish;
 use rand::{seq::IteratorRandom, thread_rng};
@@ -70,11 +69,10 @@ pub fn win32_disclaimer() {
     rect.x += 10;
     rect.w -= 10; //leave some border
     DisplayText(
-        cstr!(
-        "Windows disclaimer:\n\nThis program is 100% Free (as in Freedom), licenced under the GPL.\
+        c"Windows disclaimer:\n\nThis program is 100% Free (as in Freedom), licenced under the GPL.\
          \nIt is developed on a free operating system (GNU/Linux) using exclusively free tools. \
          For more information about Free Software see the GPL licence (in the file COPYING)\n\
-         or visit http://www.gnu.org.\n\n\n Press fire to play.")
+         or visit http://www.gnu.org.\n\n\n Press fire to play."
         .as_ptr(),
         rect.x.into(),
         rect.y.into(),
@@ -593,7 +591,7 @@ impl crate::Data<'_> {
         //
 
         self.vars.me.text_to_be_displayed =
-            TextToBeDisplayed::String(cstr!("Ok. I'm on board.  Let's get to work.")); // taken from Paradroid.mission
+            TextToBeDisplayed::String(c"Ok. I'm on board.  Let's get to work."); // taken from Paradroid.mission
         self.vars.me.text_visible_time = 0.;
 
         main_mission_data.get_crew(self);
@@ -861,8 +859,8 @@ impl crate::Data<'_> {
     /// it only receives a pointer to the start of the bullet section from
     /// the calling function.
     pub fn get_bullet_data(&mut self, data_slice: &[u8]) {
-        // const BULLET_SECTION_BEGIN_STRING: &CStr = cstr!("*** Start of Bullet Data Section: ***");
-        // const BULLET_SECTION_END_STRING: &CStr = cstr!("*** End of Bullet Data Section: ***");
+        // const BULLET_SECTION_BEGIN_STRING: &CStr = c"*** Start of Bullet Data Section: ***";
+        // const BULLET_SECTION_END_STRING: &CStr = c"*** End of Bullet Data Section: ***";
         const NEW_BULLET_TYPE_BEGIN_STRING: &[u8] =
             b"** Start of new bullet specification subsection **";
 
@@ -872,7 +870,7 @@ impl crate::Data<'_> {
         const BULLET_DAMAGE_BEGIN_STRING: &[u8] = b"Damage cause by a hit of this bullet type :";
         // #define BULLET_NUMBER_OF_PHASES_BEGIN_STRING "Number of different phases that were designed for this bullet type :"
         // const BULLET_ONE_SHOT_ONLY_AT_A_TIME: &CStr =
-        //     cstr!("Cannot fire until previous bullet has been deleted : ");
+        //     c"Cannot fire until previous bullet has been deleted : ";
 
         const BULLET_SPEED_CALIBRATOR_STRING: &[u8] =
             b"Common factor for all bullet's speed values: ";
@@ -956,9 +954,9 @@ impl crate::Data<'_> {
     /// a dat file, that should be optimally human readable.
     pub fn get_general_game_constants(&mut self, data: &[u8]) {
         // const CONSTANTS_SECTION_BEGIN_STRING: &CStr =
-        //     cstr!("*** Start of General Game Constants Section: ***");
+        //     c"*** Start of General Game Constants Section: ***";
         // const CONSTANTS_SECTION_END_STRING: &CStr =
-        //     cstr!("*** End of General Game Constants Section: ***");
+        //     c"*** End of General Game Constants Section: ***";
         const COLLISION_LOSE_ENERGY_CALIBRATOR_STRING: &[u8] =
             b"Energy-Loss-factor for Collisions of Influ with hostile robots=";
         const BLAST_RADIUS_SPECIFICATION_STRING: &[u8] =

commit d4652b9abc762afef94414cc1d05fddc06d72a86

fix: android compilation
Regression caused in e34e3f3955031447ca6ae4d5675cf92e0f1ccacd by
accidentally _shifting_ conditional compilation attributes that should
be removed instead.

freedroid/src/input.rs
@@ -7,7 +7,6 @@ use crate::{
     vars::Vars,
 };
 
-#[cfg(not(target_os = "android"))]
 use log::info;
 use sdl::{
     Event, Joystick,

freedroid/src/map.rs
@@ -16,7 +16,6 @@ use crate::{
 
 use arrayvec::ArrayVec;
 use bstr::ByteSlice;
-#[cfg(not(target_os = "android"))]
 use defs::MAX_DOORS_ON_LEVEL;
 #[cfg(not(target_os = "android"))]
 use defs::MAX_WP_CONNECTIONS;